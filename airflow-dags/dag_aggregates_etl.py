dcc8b6001ec54f059cc68ca40adbbbb96b2c72f8
Matthew Hanni <22209368+MatthewHanni@users.noreply.github.com>
2022-12-10 15:13:29 -0500

    Earlier version of this code used ETL into a database. Updated version writes aggregates data to S3 and the DAG uses the data in S3 as a cursor for instruction on incremental loading to the ETL in the ECS container.

--- airflow-dags/dag_aggregates_etl.py
+++ airflow-dags/dag_aggregates_etl.py
@@ -5,12 +5,14 @@ import time
 from airflow.models import Variable
 import boto3
 from sqlalchemy import create_engine
+import json
 
 default_args = {
-    "owner":"hanni",
-    "retries":0,
-    "retry_delay":datetime.timedelta(minutes=5)
+    "owner": "hanni",
+    "retries": 0,
+    "retry_delay": datetime.timedelta(minutes=5)
 }
+
 
 def get_db_connection():
     DATABASE_USER = Variable.get("RDS_INFERNO_DATABASE_USER")
@@ -23,32 +25,110 @@ def get_db_connection():
     connection = engine.connect()
     return connection
 
+
 def get_ticker_list(conn):
     ticker_list = []
 
-    ticker_list = ['UPS','SNOW','YUM']
-    return ticker_list
+    # ticker_list = ['AAPL']
+    # return ticker_list
 
     query = """SELECT ticker FROM reference_db.tickers WHERE market = 'stocks'"""
     results = conn.execute(query).fetchall()
     for result in results:
         ticker_list.append(result[0])
+
+    ticker_list = ticker_list[:128]
+
     return ticker_list
 
-def get_latest_record_information(ticker,conn):
+
+def get_most_recent_key(ticker, s3_client, bucket_name):
+    database_name = 'aggregates_adjusted'
+    print(f'Looking for latest record for {ticker}')
+    bucket_name = Variable.get("AGGREGATES_BUCKET")
+    print(f'Bucket name set:{ticker}')
+    prefix = f'{database_name}/{ticker}/'
+    print(f'Prefix set:{prefix}')
+    all_keys = get_key_list(s3_client=s3_client, bucket_name=bucket_name, prefix=prefix)
+    if len(all_keys) == 0:
+        return None
+    max_date = None
+    max_key = None
+    for key_object in all_keys:
+        last_modified = key_object['LastModified']
+        key = key_object['Key']
+        if max_date is None or last_modified > max_date:
+            max_date = last_modified
+            max_key = key
+    print(f'Latest key:{max_key}\tLatest date:{max_date}')
+    return max_key
+
+
+def get_key_list(s3_client, bucket_name, prefix):
+    all_keys = []
+    next_continuation_token = None
+    while True:
+        print('Looking for keys...')
+        response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=prefix) if next_continuation_token is None \
+            else s3_client.list_objects_v2(Bucket=bucket_name, Prefix=prefix, ContinuationToken=next_continuation_token)
+        if 'Contents' in response:
+            all_keys.extend(response['Contents'])
+            print(f'Found {len(response["Contents"])} keys (Total:{len(all_keys)})')
+
+        if 'NextContinuationToken' in response:
+            next_continuation_token = response['NextContinuationToken']
+            print(next_continuation_token)
+        else:
+            break
+    print(f'Total keys found {len(all_keys)}')
+    return all_keys
+
+
+def get_latest_key_record_offsets(s3_client, bucket_name, key):
+    s3_client.download_file(bucket_name, key, 'tmp.json')
+    print('Downloaded key')
+    latest_entry_timestamp = None
+    with open('tmp.json') as f:
+        data = json.load(f)
+        for record in data['results']:
+            t = record['t']
+            if latest_entry_timestamp is None or t > latest_entry_timestamp:
+                latest_entry_timestamp = t
+    latest_entry_timestamp = str(latest_entry_timestamp)
+    return latest_entry_timestamp
+
+
+def get_latest_record_information(ticker, conn):
+    bucket_name = Variable.get("AGGREGATES_BUCKET")
+    aws_access_key_id = Variable.get("AWS_ACCESS_KEY_ID")
+    aws_secret_access_key = Variable.get("AWS_SECRET_ACCESS_KEY")
+    region_name = Variable.get("AWS_REGION_NAME")
+    print('Making connection to S3')
+    s3_client = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key,
+                             region_name=region_name)
+    most_recent_key = get_most_recent_key(ticker, s3_client, bucket_name)
+    if most_recent_key is None:
+        return None
+
+    latest_entry_timestamp = get_latest_key_record_offsets(s3_client=s3_client, bucket_name=bucket_name,
+                                                           key=most_recent_key)
+
+    print(f'Most recent entry timestamp{latest_entry_timestamp}')
+    return latest_entry_timestamp
+
+
+def get_latest_record_information_db(ticker, conn):
     try:
         results = conn.execute(
             f'SELECT max(datetime),max(t) FROM aggregates_adjusted.{ticker} order by t desc').fetchall()
-        last_entry_date = str(results[0][0].date())
+
         last_entry_timestamp = str(results[0][1])
-        return last_entry_date, last_entry_timestamp
+        return last_entry_timestamp
     except:
-        return None, None
+        return None
 
 
-
-def process_ticker(ticker, conn,ecs_client):
-
+def process_ticker(ticker, conn, ecs_client):
     cluster = Variable.get("ECS_CLUSTER_POLYGON_ARN")
     task_definition = Variable.get("ECS_CLUSTER_TASK_DEFINITION_AGGREGATES")
     AGGREGATES_API_KEY = Variable.get("POLYGON_AGGREGATES_API_KEY")
@@ -58,11 +138,18 @@ def process_ticker(ticker, conn,ecs_client):
     DATABASE_HOST = Variable.get("RDS_INFERNO_DATABASE_HOST")
     DATABASE_PORT = Variable.get("RDS_INFERNO_DATABASE_PORT")
     DATABASE_DBNAME = Variable.get("RDS_INFERNO_DATABASE_DBNAME")
-    last_entry_date, last_entry_timestamp = get_latest_record_information(ticker=ticker,conn=conn)
+
+    aws_access_key_id = Variable.get("AWS_ACCESS_KEY_ID")
+    aws_secret_access_key = Variable.get("AWS_SECRET_ACCESS_KEY")
+    region_name = Variable.get("AWS_REGION_NAME")
+    bucket_name = Variable.get("AGGREGATES_BUCKET")
+
     timespan = 'minute'
     multiplier = '5'
 
-    if last_entry_date is not None and last_entry_timestamp is not None:
+    latest_entry_timestamp = get_latest_record_information(ticker=ticker, conn=conn)
+
+    if latest_entry_timestamp is not None:
         overrides = {
             'containerOverrides': [
                 {'name': 'aggregates_etl',
@@ -98,11 +185,20 @@ def process_ticker(ticker, conn,ecs_client):
                          'name': 'DATABASE_DBNAME',
                          'value': DATABASE_DBNAME
                      }, {
-                         'name': 'LAST_ENTRY_DATETIME',
-                         'value': last_entry_date
+                         'name': 'LATEST_ENTRY_TIMESTAMP',
+                         'value': latest_entry_timestamp
                      }, {
-                         'name': 'LAST_ENTRY_TIMESTAMP',
-                         'value': last_entry_timestamp
+                         'name': 'AWS_ACCESS_KEY',
+                         'value': aws_access_key_id
+                     }, {
+                         'name': 'AWS_SECRET_KEY',
+                         'value': aws_secret_access_key
+                     }, {
+                         'name': 'DATABRICKS_BUCKET',
+                         'value': bucket_name
+                     }, {
+                         'name': 'DATABRICKS_BUCKET_REGION',
+                         'value': region_name
                      },
                  ],
 
@@ -144,6 +240,18 @@ def process_ticker(ticker, conn,ecs_client):
                      }, {
                          'name': 'DATABASE_DBNAME',
                          'value': DATABASE_DBNAME
+                     }, {
+                         'name': 'AWS_ACCESS_KEY',
+                         'value': aws_access_key_id
+                     }, {
+                         'name': 'AWS_SECRET_KEY',
+                         'value': aws_secret_access_key
+                     }, {
+                         'name': 'DATABRICKS_BUCKET',
+                         'value': bucket_name
+                     }, {
+                         'name': 'DATABRICKS_BUCKET_REGION',
+                         'value': region_name
                      },
                  ],
 
@@ -165,17 +273,14 @@ def process_ticker(ticker, conn,ecs_client):
             break
     print(status)
 
-def etl():
 
+def etl():
     print('Making connection to boto3')
     access_key_id = Variable.get("AWS_ACCESS_KEY_ID")
     secret_access_key = Variable.get("AWS_SECRET_ACCESS_KEY")
     region = Variable.get("ECS_CLUSTER_POLYGON_REGION")
     ecs_client = boto3.client('ecs', aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key,
-                          region_name=region)
-
-
-
+                              region_name=region)
 
     print('Connecting to database...')
     conn = get_db_connection()
@@ -185,25 +290,22 @@ def etl():
 
     for i in range(len(ticker_list)):
         ticker = ticker_list[i]
-        print(f'Processing ticker {i+1}:{len(ticker_list)} {ticker}')
-        process_ticker(ticker,conn,ecs_client)
-
-
+        print(f'Processing ticker {i + 1}:{len(ticker_list)} {ticker}')
+        process_ticker(ticker, conn, ecs_client)
 
     print('DAG complete')
 
 
-
 with DAG(
-    default_args=default_args,
-    dag_id='dag_aggregates',
-    description="DAG: ETL for aggregates",
-    start_date=datetime.datetime(2022,12,6,16,30,0),
-    schedule_interval='@daily'
+        default_args=default_args,
+        dag_id='dag_aggregates',
+        description="DAG: ETL for aggregates",
+        start_date=datetime.datetime(2022, 12, 6, 16, 30, 0),
+        schedule_interval='@daily'
 ) as dag:
     etl_task = PythonOperator(
         task_id="etl",
         python_callable=etl,
-        op_kwargs={}
+        provide_context=True
     )
     etl_task
--- etl-containers/aggregates/src/aggregates_etl.py
+++ etl-containers/aggregates/src/aggregates_etl.py
@@ -5,9 +5,13 @@ import pandas as pd
 import os
 import pytz
 from sqlalchemy import create_engine
+import json
+import boto3
+import random
+import string
 
-DEFAULT_START_DATE = '1990-01-01'
-RATE_LIMIT_TIMEOUT = .1
+DEFAULT_START_DATE = datetime.datetime(year=2018, month=1, day=1)
+RATE_LIMIT_TIMEOUT = 1
 
 ticker = os.environ.get('TICKER')
 api_key = os.environ.get(f'AGGREGATES_API_KEY')
@@ -17,9 +21,17 @@ database_host = os.environ.get('DATABASE_HOST')
 database_port = os.environ.get('DATABASE_PORT')
 multiplier = os.environ.get('MULTIPLIER')
 timespan = os.environ.get('TIMESPAN')
-last_entry_date = os.environ.get('LAST_ENTRY_DATE')
-last_entry_timestamp = os.environ.get('LAST_ENTRY_TIMESTAMP')
+
+latest_entry_timestamp = os.environ.get('LATEST_ENTRY_TIMESTAMP')
 adjusted = os.environ.get('ADJUSTED')
+aws_access_key_id = os.environ.get('AWS_ACCESS_KEY')
+aws_secret_access_key = os.environ.get('AWS_SECRET_KEY')
+bucket_name = os.environ.get('DATABRICKS_BUCKET')
+region_name = os.environ.get('DATABRICKS_BUCKET_REGION')
+tmp_path = 'tmp.json'
+
+s3_client = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key,
+                         region_name=region_name)
 
 limit = 50000
 
@@ -31,19 +43,13 @@ else:
     print(f'Unknown adjusted type:{type(adjusted)}\tValue:{adjusted}')
     exit()
 
-if last_entry_date is None:
-    from_ = DEFAULT_START_DATE
+if latest_entry_timestamp is None:
+    from_dt = DEFAULT_START_DATE
 else:
-    from_ = last_entry_date
+    latest_entry_timestamp = int(latest_entry_timestamp)
+    from_dt = datetime.datetime.fromtimestamp(latest_entry_timestamp / 1000.0)
 
-to_ = datetime.date.today()
 table_name = ticker.upper()
-
-if type(last_entry_timestamp) is str:
-    try:
-        last_entry_timestamp = int(last_entry_timestamp)
-    except:
-        last_entry_timestamp = None
 
 
 def ts_to_datetime(ts) -> str:
@@ -51,10 +57,20 @@ def ts_to_datetime(ts) -> str:
     return x
 
 
+def get_nonce(length=5):
+    letters = string.ascii_uppercase + string.digits
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
+
+
+to_ = str(datetime.date.today())
 all_results = []
+from_ = str(from_dt.date())
 while True:
 
+
     url = f'https://api.polygon.io/v2/aggs/ticker/{ticker}/range/{multiplier}/{timespan}/{from_}/{to_}?adjusted={adjusted}&sort=asc&limit={limit}&apiKey={api_key}'
+
     response = requests.get(url)
 
     if response.status_code != 200:
@@ -62,11 +78,20 @@ while True:
 
     response_json = response.json()
 
+    with open(tmp_path, 'w') as f:
+        json.dump(response_json, f)
+
+    with open(tmp_path, 'rb') as f:
+        timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+        s3_path = f'{database_name}/{ticker}/{ticker}--{timestamp}--{get_nonce()}.json'
+        s3_client.upload_fileobj(f, bucket_name, s3_path)
+
+    prev_count = -1
     for i in range(len(response_json['results'])):
         t = response_json['results'][i]['t']
 
         # Check if the max timestamp in our DB is greater than this result record. If so, we've likely already captured it.
-        if last_entry_timestamp is not None and last_entry_timestamp >= t:
+        if latest_entry_timestamp is not None and latest_entry_timestamp >= t:
             continue
 
         # Check if the latest record in memory is greater than this result record. If so, we've likely already captured it.
@@ -76,22 +101,39 @@ while True:
         response_json['results'][i]['datetime'] = ts_to_datetime(t)
         all_results.append(response_json['results'][i])
 
-    # If the number of results returned was less than the max number of values we expect back, then we are up-to-date
-    if len(response_json['results']) < limit:
+    # # This assumption is incorrect. The API does not work this way.
+    # ERROR: If the number of results returned was less than the max number of values we expect back, then we are up-to-date
+    # if len(response_json['results']) < limit:
+    #    break
+
+    # If we've returned 0 results, we are up-to-date
+    if response_json['resultsCount'] == 0:
         break
+
+    # If we haven't added any new records
+    if prev_count == len(all_results):
+        break
+
+    prev_count = len(all_results)
 
     latest_result_timestamp = response_json['results'][-1]['t']
     latest_result_datetime = ts_to_datetime(latest_result_timestamp)
+    latest_result_date = str(latest_result_datetime).split(' ')[0]
 
-    from_ = str(latest_result_datetime).split(' ')[0]
+
+    if from_ == latest_result_date:
+        break
+    else:
+        from_ = latest_result_date
+
     time.sleep(RATE_LIMIT_TIMEOUT)
 
-df = pd.DataFrame(all_results)
+# df = pd.DataFrame(all_results)
 
-database_url = f'mysql://{database_user}:{database_password}@{database_host}:{database_port}/{database_name}'
-engine = create_engine(database_url, echo=True)
-connection = engine.connect()
+# database_url = f'mysql://{database_user}:{database_password}@{database_host}:{database_port}/{database_name}'
+# engine = create_engine(database_url, echo=True)
+# connection = engine.connect()
 
-df['insert_timestamp'] = datetime.datetime.now(pytz.timezone('US/Eastern'))
+# df['insert_timestamp'] = datetime.datetime.now(pytz.timezone('US/Eastern'))
 
-df.to_sql(table_name, con=engine, if_exists='append')
+# df.to_sql(table_name, con=engine, if_exists='append')
